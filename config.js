30a1fc394748a42912c63e4f722014d72263cd5c
ALUCARD-BOT25 <tplayertoxic@gmail.com>
2025-05-28 19:01:39 -0400

    config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "pur your session id here ",
+SESSION_ID: process.env.SESSION_ID || "GOTAR~XMD~jBRl3JqY#1s3YPsx2rT-BmLz3bRfyJB2eFjtGMwCqZ3e64O3y2NY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
